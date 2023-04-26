8febfbb0f65fbd653963cb9885eef1d654ebec57
sds <btv48817@nezid.com>
2023-04-26 18:00:37 +0700

    Continue

--- mysite/myapp/admin.py
+++ mysite/myapp/admin.py
@@ -3,4 +3,6 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 admin.site.register(Chawo)
+admin.site.register(Goods)
+admin.site.register(Vallet)
 # Register your models here.
--- mysite/myapp/models.py
+++ mysite/myapp/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 class Chawo(models.Model):
@@ -10,6 +11,32 @@ class Chawo(models.Model):
 
 
 
+class Goods(models.Model):
+    Name = models.TextField(max_length=51)
+    cost = models.IntegerField(default=0)
+    rate=models.IntegerField(default=5)
+    image=models.TextField(max_length=100)
+    owner=models.ForeignKey(User, on_delete=models.CASCADE,)
+    sold_type=models.BooleanField(default=False)
+    resourse_typee=models.SmallIntegerField(default=0)
+
+    def __str__(self):
+        return f'name: {self.Name}, ' \
+               f'cost: {self.cost}, '\
+               f'owner: {self.owner}'\
+               f'rate: {self.rate}'
+
+
+class Vallet(models.Model):
+    web=models.TextField(max_length=100)
+    money=models.IntegerField(default=0)
+    vallet_number=models.TextField(max_length=200)
+    owner=models.ForeignKey(User, on_delete=models.CASCADE,)
+
+    def __str__(self):
+        return f'web: {self.web}, ' \
+               f'money: {self.money}, '\
+               f'vallet_number: {self.vallet_number}'\
 
 
 
