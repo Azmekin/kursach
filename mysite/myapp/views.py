df4f4683ef13802c3a9be52182863042b0d6a103
sds <btv48817@nezid.com>
2023-04-26 17:34:00 +0700

    Continue

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -11,13 +11,14 @@ from .serializers import ChawoSerializer, Chawo
 
 class ChawoView(generics.ListAPIView):
     serializer_class = ChawoSerializer
-    queryset = Chawo.objects.all()
 
 class TestTemplate(generics.ListAPIView):
     renderer_classes = [TemplateHTMLRenderer]
     template_name = 'profile_list.html'
 
     def get(self, request):
+       a = request.query_params
        queryset = Chawo.objects.all()
-       return render(request,'index.html',{'posts':queryset,'answ':'TODO'})
+       a=Chawo.objects.filter(id=a['a'])
+       return render(request,'index.html',{'posts':queryset,'answ':a})
        #Response({'profiles': queryset})
